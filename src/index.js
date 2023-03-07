822cb4d4d00c05d31ffc1cfde12461bdefb4ed6b
azne <a_kurbanov@hotmail.com>
2023-03-07 21:35:44 +0100

    update

---
+++ src/index.js
@@ -0,0 +1,49 @@
+import { initializeApp } from "firebase/app";
+import { 
+getFirestore, collection, getDocs, doc
+
+} from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDvAy7nj6jiWQV1_H5JOeEJC_O4WJ7fjnA",
+  authDomain: "fir-9-b84b8.firebaseapp.com",
+  projectId: "fir-9-b84b8",
+  storageBucket: "fir-9-b84b8.appspot.com",
+  messagingSenderId: "601970819058",
+  appId: "1:601970819058:web:8982aaba026ef2d6c1756b",
+};
+
+//init firebase app
+initializeApp(firebaseConfig);
+
+//init services
+const db = getFirestore();
+
+//collection ref
+const colRef = collection(db, 'books');
+
+//get collection data
+getDocs(colRef)
+.then((snapshot)=>{
+let books = []
+snapshot.docs.forEach((doc)=>{
+    books.push({...doc.data(), id: doc.id})
+})
+console.log(books);
+})
+.catch(err=>{
+    console.log(err.message)
+});
+
+
+//adding documents
+const addBookForm = document.querySelector('.add')
+addBookForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+});
+
+//deleting documents
+const deleteBookForm = document.querySelector('.delete')
+deleteBookForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+});
