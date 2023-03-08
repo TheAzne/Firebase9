558ad99b0efb8f50d60f1e954f77c0d894b28628
azne <a_kurbanov@hotmail.com>
2023-03-08 12:47:06 +0100

    create signIn and signOut

--- src/index.js
+++ src/index.js
@@ -1,10 +1,24 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, onSnapshot, addDoc,
-deleteDoc,doc, query, where, orderBy, serverTimestamp,getDoc, updateDoc } from "firebase/firestore";
-import{
-    getAuth,
-    createUserWithEmailAndPassword,
-} from 'firebase/auth';
+import {
+  getFirestore,
+  collection,
+  onSnapshot,
+  addDoc,
+  deleteDoc,
+  doc,
+  query,
+  where,
+  orderBy,
+  serverTimestamp,
+  getDoc,
+  updateDoc,
+} from "firebase/firestore";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signOut,
+  signInWithEmailAndPassword,
+} from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyDvAy7nj6jiWQV1_H5JOeEJC_O4WJ7fjnA",
@@ -26,16 +40,16 @@ const auth = getAuth();
 const colRef = collection(db, "books");
 
 //queries
-const q = query(colRef, orderBy('createdAt'));
+const q = query(colRef, orderBy("createdAt"));
 
 //real time collection data
-  onSnapshot(q,(snapshot)=>{
-    let books = [];
-    snapshot.docs.forEach((doc) => {
-      books.push({ ...doc.data(), id: doc.id });
-    });
-    console.log(books);
-  })
+onSnapshot(q, (snapshot) => {
+  let books = [];
+  snapshot.docs.forEach((doc) => {
+    books.push({ ...doc.data(), id: doc.id });
+  });
+  console.log(books);
+});
 
 //adding documents
 const addBookForm = document.querySelector(".add");
@@ -56,52 +70,75 @@ const deleteBookForm = document.querySelector(".delete");
 deleteBookForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  const docRef = doc(db,'books',deleteBookForm.id.value)
+  const docRef = doc(db, "books", deleteBookForm.id.value);
 
-  deleteDoc(docRef)
-  .then(()=>{
+  deleteDoc(docRef).then(() => {
     deleteBookForm.reset();
   });
-
 });
 
 //get a single document
 
-const docRef =  doc(db, 'books', '4g7NKYB3evjG2Xm3XzZH')
-onSnapshot(docRef, (doc)=>{
-    console.log(doc.data(), doc.id)
-})
-
+const docRef = doc(db, "books", "4g7NKYB3evjG2Xm3XzZH");
+onSnapshot(docRef, (doc) => {
+  console.log(doc.data(), doc.id);
+});
 
 //update a document
 const updateForm = document.querySelector(".update");
-updateForm.addEventListener("submit", (e)=>{
-    e.preventDefault();
+updateForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    const docRef = doc(db, 'books', updateForm.id.value);
+  const docRef = doc(db, "books", updateForm.id.value);
 
-    updateDoc(docRef,{
-        title: 'updated title',
-    })
-    .then(()=>{
-        updateDoc.reset();
-    })
-})
+  updateDoc(docRef, {
+    title: "updated title",
+  }).then(() => {
+    updateDoc.reset();
+  });
+});
 
 //signing users up
-const signupForm = document.querySelector('.signup');
-signupForm.addEventListener('submit',(e)=>{
-    e.preventDefault();
+const signupForm = document.querySelector(".signup");
+signupForm.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-    const email = signupForm.email.value;
-    const password = signupForm.password.value;
+  const email = signupForm.email.value;
+  const password = signupForm.password.value;
 
-    createUserWithEmailAndPassword(auth,email, password)
-    .then((cred)=>{
-        console.log('user created:',cred.user);
-        signupForm.reset();
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((cred) => {
+      console.log("user created:", cred.user);
+      signupForm.reset();
     })
-    .catch((err)=>{
-        console.log(err.message)
+    .catch((err) => {
+      console.log(err.message);
+    });
+});
+
+//logging in and out
+const logoutButton = document.querySelector(".logout");
+logoutButton.addEventListener("click", () => {
+  signOut(auth)
+    .then(() => {
+      console.log("The user signed out");
     })
-})
+    .catch((err) => {
+      console.log(err.message);
+    });
+});
+
+const loginButton = document.querySelector(".login");
+loginButton.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const email = loginButton.email.value;
+  const password = loginButton.password.value;
+  signInWithEmailAndPassword(auth, email, password)
+  .then((cred)=>{
+    console.log('User logged in: ',cred.user)
+  })
+  .catch((err)=>{
+    console.log(err.message)
+  });
+});
